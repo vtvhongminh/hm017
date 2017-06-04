f76b2e01bf62cb3c017ce61cef96783c94f63a53
HM <HM@HM>
2017-06-04 23:53:51 +0700

    ?

--- Minhphgc00308/src/Managerment_pd/Main_pd.java
+++ Minhphgc00308/src/Managerment_pd/Main_pd.java
@@ -19,20 +19,16 @@ public class Main_pd {
 
 	public static void runproduct() {
 		ModulProduct m = new ModulProduct();
-		m.dataFile = Res.getDesktopFile("dsa2017-data/1e5/products.json");
+		m.dataFile = Res.getDesktopFile("dsa2017-data/1e2/products.json");
 		m.inputData();
-		// m.removeByNote("PRDAAAAAB");
-		// m.removeByAfNode("PRDAAAAAA");
-		// m.searchByPcode("PRDAAAAAA");
-		// m.sortByPcode();
-		// m.displayData();
+	
 
 		ModulProduct.displayDataSwing("Product");
 		// m.SavePdl();
 	}
 	public static void runcus() {
 		ModulCus mc= new ModulCus();
-		mc.dataFile = Res.getDesktopFile("dsa2017-data/1e5/customers.json");
+		mc.dataFile = Res.getDesktopFile("dsa2017-data/1e2/customers.json");
 		mc.inputData();
 	
 
@@ -41,7 +37,7 @@ public class Main_pd {
 	}
 	public static void runorder() {
 		ModulOrder mc = new ModulOrder();
-		mc.dataFile = Res.getDesktopFile("dsa2017-data/1e5/orders.json");
+		mc.dataFile = Res.getDesktopFile("dsa2017-data/1e2/orders.json");
 		mc.inputData();
 
 
